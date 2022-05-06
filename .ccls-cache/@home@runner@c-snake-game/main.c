2c40a08b26227ad00c0866de90fab4bc08af7eee
Arthur Ramon Souza <83590077+ArthurRamonSouza@users.noreply.github.com>
2022-05-06 23:47:32 +0000

    Randomly generating the food

--- .ccls-cache/@home@runner@c-snake-game/main.c
+++ .ccls-cache/@home@runner@c-snake-game/main.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define R 20
 #define C 60
 
-int column, row, head, tail, snake_x, snake_y, game;
+// Main variables
+int column, row, head, tail, snake_x, snake_y, game, apple, apple_x, apple_y;
 int fixBug = 0; // THe bottom is duplicated without this
 int Field[C][R];
 
 void snakeInitialization() {
 
-  // Snake starting position, the head will be placed in coordinates
-  // [snake_x][snake_y]
+  // Snake starting position, the head will be placed in coordinates[snake_x][snake_y]
   snake_x = C / 2;
   snake_y = R / 2;
   head = 4;
   tail = 1;
   game = 0;
-  
-  
+  apple_x = 1 + rand() % 18;
+  apple_y = 1 + rand() % 58;
+
   // Putting the snake in the field
   for (row = 1; row <= snake_y; row++) {
     for (column = 1; column <= snake_x; column++) {
       if (row == snake_y && column == snake_x - head) {
         int node = tail;
         while (node <= head) {
+          system("setterm -foreground yellow");
           Field[column][row] = node;
           node++;
           column++;
         }
       } else {
-        // Assigning value of 0 to empty spaces
-        Field[column][row] = 0;
-      }
+          // Assigning value of 0 to empty spaces
+          Field[column][row] = 0; 
+        }
     }
   }
 }
@@ -52,14 +55,17 @@ void makeTopAndBottom() {
 void createField() {
   makeTopAndBottom();
 
-  for (row = 1; row <= R; row++) {
+  for (row = 1; row < R; row++) {
     for (column = 0; column <= C + 1; column++) {
       if (column == 0 || column == C + 1) {
+        system("setterm -foreground green");
         printf("%c", 43);
       } else if (Field[column][row] == 0) {
         printf(" ");
       } else if (Field[column][row] > 0 && Field[column][row] != head) {
         printf("%c", 111);
+      } else if (Field[column][row] == -1) {
+        printf("%c", 64);
       } else if (Field[column][row] == head) {
         printf("%c", 62);
       }
@@ -69,29 +75,43 @@ void createField() {
   makeTopAndBottom();
 }
 
-void resetScreenPosition(){
+void resetScreenPosition() {
   printf("\033[%d;%dH", 0, 0);
   // THe bottom is duplicated without this
-  if (fixBug == 0){
+  if (fixBug == 0) {
     system("clear");
     fixBug = 1;
   }
 }
 
+void foodGenerator() {
+  srand(time(0)); // Generating random numbers from the time
+  apple_x = rand() % R;
+  apple_y = rand() % C;
+
+  // If there is no apple on the screen and the space is empty, the space gets -1
+  if (apple == 0 && Field[apple_x][apple_y] == 0) {
+    Field[apple_x][apple_y] = -1;
+    apple = 1;
+  }
+}
+
 int main(void) {
   snakeInitialization();
-  
-  while (game == 0){
-    createField();
-    resetScreenPosition();
-  }
-  
-  for (row = 1; row <= R; row++) {
+
+  for (row = 1; row < R; row++) {
     for (column = 1; column <= C; column++) {
       int array = Field[column][row];
       printf("%d", array);
     }
     printf("\n");
   }
+
+  while (game == 0) {
+    createField();
+    resetScreenPosition();
+    foodGenerator();
+  }
+
   return 0;
 }
